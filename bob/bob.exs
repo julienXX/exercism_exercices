d4ba150db36c488a6c20c9618fbb1d2c74093313
Julien Blanchard <julien@sideburns.eu>
2013-07-27 19:31:47 +0200

    Make is_ functions private, use pipeline operator for is_a_question?

--- bob/bob.exs
+++ bob/bob.exs
@@ -13,17 +13,17 @@ defmodule Teenager do
     end
   end
 
-  def is_nil?(string) do
+  defp is_nil?(string) do
     # first/1 returns nil is the string is empty
     nil? String.first string
   end
 
-  def is_upcase?(string) do
+  defp is_upcase?(string) do
     string == String.upcase(string)
   end
 
-  def is_a_question?(string) do
-    String.ends_with? string, "?"
+  defp is_a_question?(string) do
+    string |> String.ends_with? "?"
   end
 
 end
