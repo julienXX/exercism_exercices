ad34b88adba41c1c3ee2119a95f519d1e75897ad
Julien Blanchard <julien@sideburns.eu>
2013-07-31 12:26:37 +0200

    Add word-count

---
+++ elixir/word-count/word-count.exs
@@ -0,0 +1,31 @@
+defmodule Words do
+
+  @doc """
+    Takes a string and returns a HashDict with the number of occurences for each word.
+
+    iex> Words.count("foo bar")
+    HashDict.new [{ "foo", 1 }, { "bar", 1 }]
+  """
+
+  def count(sentence) do
+    dict = HashDict.new []
+    String.downcase(sentence) |> String.split |>  do_count(dict)
+  end
+
+  defp do_count([], dict), do: dict
+  defp do_count([head|tail], dict) do
+    entry = prepare_entry(head, dict)
+    do_count(tail, entry)
+  end
+
+  defp prepare_entry(word, dict) do
+    Regex.replace(%r/\W/, word, "") |> add_or_increment_entry(dict)
+  end
+
+  defp add_or_increment_entry("", dict), do: dict
+  defp add_or_increment_entry(word, dict) do
+    current_count = HashDict.get(dict, word) || 0
+    HashDict.put(dict, word, current_count + 1)
+  end
+
+end
