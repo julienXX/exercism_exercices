2cdf71d7e7be1d673216928e79df32cf27915c10
Julien Blanchard <julien@sideburns.eu>
2013-08-21 14:16:29 +0200

    Import String codepoints/1

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,5 +1,6 @@
 defmodule Anagram do
   import Enum,   only: [filter: 2, sort: 1]
+  import String, only: [codepoints: 1]
 
   @doc """
     Takes a list of strings and returns possible anagrams for a given word.
@@ -9,12 +10,12 @@ defmodule Anagram do
   """
 
   def match(word, list) do
-    exercifilter(list, fn(potential_word) -> anagram?(word, potential_word) end)
+    filter(list, fn(potential_word) -> anagram?(word, potential_word) end)
   end
 
   defp anagram?(word, potential_word) do
-    sorted_word_codepoints           = sort(String.codepoints(word))
-    sorted_potential_word_codepoints = sort(String.codepoints(potential_word))
+    sorted_word_codepoints           = sort(codepoints(word))
+    sorted_potential_word_codepoints = sort(codepoints(potential_word))
 
     sorted_word_codepoints == sorted_potential_word_codepoints
   end
