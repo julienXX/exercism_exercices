d9ed467706f77d598deee137ed89db804d58d09f
Julien Blanchard <julien@sideburns.eu>
2013-08-22 14:49:23 +0200

    Thymine and uracil have their own variables

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -1,7 +1,10 @@
 (ns dna
   (require [clojure.string :as str]))
 
+(def thymine \T)
+(def uracil  \U)
+
 (defn to-rna
   "Replace all occurrences of thymine with uracil."
   [sequence]
-  (str/replace sequence #"T" "U"))
+  (str/replace sequence thymine uracil))
