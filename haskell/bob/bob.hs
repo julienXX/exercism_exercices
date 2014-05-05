e5784f7f4ba795a3d3cf63c476f83945a1cce14b
Julien Blanchard <julien@sideburns.eu>
2014-05-05 11:38:17 +0200

    Haskell: Bob

--- haskell/bob/bob.hs
+++ haskell/bob/bob.hs
@@ -1,4 +1,18 @@
 module Bob (responseFor) where
+import Data.Char
+
+isSilence :: String -> Bool
+isSilence sentence = all isSpace sentence
+
+isYelling :: String -> Bool
+isYelling sentence = sentence == map toUpper sentence
+
+isQuestion :: String -> Bool
+isQuestion sentence = last sentence == '?'
 
 responseFor :: String -> String
-responseFor sentence = "Whatever."
+responseFor sentence
+        | isSilence sentence  = "Fine. Be that way."
+        | isYelling sentence  = "Woah, chill out!"
+        | isQuestion sentence = "Sure."
+        | otherwise           = "Whatever."
