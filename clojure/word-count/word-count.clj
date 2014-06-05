74c60a109fbf2960256765e5a91779dcea1e60f8
Julien Blanchard <julien@sideburns.eu>
2014-06-05 09:59:33 +0200

    Clojure: simplify word-count

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -4,19 +4,14 @@
 (defn- split-words
   "Split a string on spaces"
   [phrase]
-  (str/split phrase #"\s"))
+  (str/split phrase #"[^\w]+"))
 
 (defn- downcase
   "Lower-case a string"
   [phrase]
   (str/lower-case phrase))
 
-(defn- cleanup
-  "Remove non-alphanumeric chars from a string"
-  [phrase]
-  (str/replace phrase #"\W+" " "))
-
 (defn word-count
   "Count words occurence in a string"
   [phrase]
-  (frequencies (split-words (downcase (cleanup phrase)))))
+  (frequencies (split-words (downcase phrase))))
